82dadcf0eb2871f5d93c94a4ea9fd2d467204b32
Zaizai18 <zainabshahzadi77@gmail.com>
2026-04-29 03:09:07 -0400

    fixed vite config and added deploy scripts

--- Web-Dev-React/vite.config.js
+++ Web-Dev-React/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
+  base: '/Web-Dev-React-Homework/', 
   plugins: [
     react(),
     tailwindcss(), 
