bbe838b25c8d243e0112fe8763d315005e15de70
Zaizai18 <zainabshahzadi77@gmail.com>
2026-04-30 18:07:50 -0400

    changes

--- Web-Dev-React/src/components/Header.jsx
+++ Web-Dev-React/src/components/Header.jsx
@@ -27,9 +27,9 @@ export default function Header({ page, setPage, isNavOpen, setIsNavOpen, cartCou
         {page === 'menu' && (
           <button 
             onClick={() => setIsCartOpen(true)}
-            className="flex items-center gap-2 border border-white/60 px-3 py-1.5 mt-4 rounded-md font-bold hover:border-[#f07f13] relative transition-all"
+            className="flex items-center gap-2 border border-white/60 px-3 py-1.25 mt-3.5 rounded-md font-bold hover:border-[#f07f13] relative transition-all"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="currentColor" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="19" fill="currentColor" viewBox="0 0 16 16">
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg>
             <span className="hidden sm:inline">CART</span>
